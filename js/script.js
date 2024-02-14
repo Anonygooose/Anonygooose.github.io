5e50f064157a0dfa6426c36c2f09478d9c7a15d4
Anonygooose <mt3982@rit.edu>
2024-02-14 13:25:46 -0500

    Made images bigger onclick

--- js/script.js
+++ js/script.js
@@ -6,21 +6,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     navbar = document.getElementById('navbar'); // Get The NavBar
 
-    window.addEventListener('scroll',function(){
-    //on every scroll this funtion will be called
-    
-    var scrollTop = window.scrollY || document.documentElement.scrollTop;
-    //This line will get the location on scroll
-    
-    if(scrollTop > lastScrollTop){ //if it will be greater than the previous
-        navbar.style.top='-210px';
-        //set the value to the negetive of height of navbar 
-    }
-    
-    else{
-        navbar.style.top='0';
-    }
-    
-    lastScrollTop = scrollTop; //New Position Stored
+    window.addEventListener('scroll',function()
+    {
+        //on every scroll this funtion will be called
+        
+        var scrollTop = window.scrollY || document.documentElement.scrollTop;
+        //This line will get the location on scroll
+        
+        if(scrollTop > lastScrollTop){ //if it will be greater than the previous
+            navbar.style.top='-210px';
+            //set the value to the negetive of height of navbar 
+        }
+        
+        else{
+            navbar.style.top='0';
+        }
+        
+        lastScrollTop = scrollTop; //New Position Stored
     });
+
+    let original = document.querySelector("header").innerHTML;
+    let list = document.querySelectorAll("img");
+
+    document.querySelectorAll(".imgLink").forEach(button => {button.addEventListener("click", buttonClicked)});
+    
+    function buttonClicked() {
+        
+        for (let i = 0; i < list.length; i++)
+        {
+            if(list[i].classList.contains("getEnlarged"))
+            {
+                list[i].classList.remove("getEnlarged");
+                if (list[i] == this.parentElement.querySelector("img"))
+                {
+                    return;
+                }
+            }
+        }
+        let imageElement = this.parentElement.querySelector("img");
+
+        imageElement.classList.toggle("getEnlarged");
+    }
 });
