0e1348af45dc43f2658fb216fe778493527b32e7
Goose <goosetarlowski@gmail.com>
2024-08-16 10:14:51 -0400

    Remaking the WHOLE website!

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload = function () { 
+    $(function(){
+    $("#nav-placeholder").load("navbar.html");
+    });
+
+    $(function(){
+    $("#foot-placeholder").load("footer.html");
+    });
+
+
+};
+
