3ee3ba2453ca9741b07fe51aafc2cd24956862ad
Goose <goosetarlowski@gmail.com>
2024-08-18 18:51:28 -0400

    Portfolio created & descriptions added!

--- js/index.js
+++ js/index.js
@@ -6,5 +6,13 @@ window.onload = function () {
     $(function(){
     $("#foot-placeholder").load("footer.html");
     });
+
+    $('.grid').masonry({
+        // use outer width of grid-sizer for columnWidth
+        columnWidth: '.grid-sizer',
+        // do not use .grid-sizer in layout
+        itemSelector: '.grid-item',
+        percentPosition: true,
+    });
 };
 
