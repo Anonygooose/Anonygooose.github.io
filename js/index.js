000ff2a53e799f591c31787e466bb1f8f5161f9c
Goose <goosetarlowski@gmail.com>
2024-08-19 19:36:16 -0400

    Gave up on lazyload

--- js/index.js
+++ js/index.js
@@ -1,33 +1,30 @@
-
-$(function() {
-    $('.lazy').Lazy();
-});
-
-$('.lazy').Lazy({
-    // your configuration goes here
-    scrollDirection: 'vertical',
-    effect: 'fadeIn',
-    visibleOnly: true,
-    onError: function(element) {
-        console.log('error loading ' + element.data('src'));
-    }
-});
-
-window.onload = function () { 
+$(window).load(function(){ 
     $(function(){
-    $("#nav-placeholder").load("navbar.html");
+        $("#nav-placeholder").load("navbar.html");
     });
 
     $(function(){
-    $("#foot-placeholder").load("footer.html");
+        $("#foot-placeholder").load("footer.html");
     });
 
-    $('.grid').masonry({
+    var $boxes = $('div.lazy');
+    $boxes.hide();
+  
+    var $container = $('.grid');
+    $container.imagesLoaded( function() {
+      $boxes.fadeIn();
+  
+      $container.masonry({
         // use outer width of grid-sizer for columnWidth
         columnWidth: '.grid-sizer',
         // do not use .grid-sizer in layout
         itemSelector: '.grid-item',
         percentPosition: true,
+      });    
     });
-};
 
+    setTimeout(function(){
+        let loadScreen = document.getElementById("loadContent");
+        loadScreen.classList.add('fade-out')
+    }, 1000);
+});
