d0a85664a744e57031cdd99d225b350ad10b5b0b
Goose <goosetarlowski@gmail.com>
2024-08-23 18:57:37 -0400

    load screen fix

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ $(function(){
     $("#loader-placeholder").load("loader.html")
 });
 
+setTimeout(function(){
+    let loadScreen = document.getElementById("loadContent");
+    loadScreen.classList.add('fade-out');
+}, 1000);
+
 $(window).load(function(){ 
     $(function(){
         $("#nav-placeholder").load("navbar.html");
@@ -19,19 +24,11 @@ $(window).load(function(){
       $boxes.fadeIn();
   
       $container.masonry({
-        // use outer width of grid-sizer for columnWidth
         columnWidth: '.grid-sizer',
-        // do not use .grid-sizer in layout
         itemSelector: '.grid-item',
         percentPosition: true,
       });    
     });
-
-    setTimeout(function(){
-        let loadScreen = document.getElementById("loadContent");
-        loadScreen.classList.add('fade-out');
-    }, 1000);
-
     
     setTimeout(function(){
         let loadScreen = document.getElementById("loadContent");
